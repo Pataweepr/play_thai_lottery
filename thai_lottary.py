a0814957d788d7744dde85bc16a0318150f9ed25
Pataweepr <39895193+Pataweepr@users.noreply.github.com>
2023-04-25 15:51:42 +0700

    Add files via upload

---
+++ thai_lottary.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Lottary_reward:
+    def __init__(self) -> None:
+        self.reward = 10000
+        self.ticket = Lottary_ticket()
+        
+        
+class Lottary_ticket:
+    def  __init__(self) -> None:
+        pass
+        
